f58e84bce8e1f9c647c831baebc72073aa306b97
enxec <dev21mo@gmail.com>
2026-04-19 17:51:03 +0900

    블로그 소개 문구를 실무·테크 지식 중심으로 수정

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 export const SITE_TITLE = 'PALDYN 기술블로그';
 export const SITE_BRAND = 'PALDYN';
 export const SITE_SUFFIX = '기술블로그';
-export const SITE_DESCRIPTION = '함께하는 힘, 팔딘. 기술과 팀이 만들어가는 기록.';
+export const SITE_DESCRIPTION = '함께하는 힘, 팔딘. 실무 기록과 테크 지식을 함께 쌓는 기술 아카이브.';
 export const SITE_AUTHOR = 'PALDYN';
 
 export const OFFICIAL_URL = 'https://paldyn.com';
